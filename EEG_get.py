d5d1343088ee32d827d9cffd687d63dcf50daede
odrum428 <oxkeita@gmail.com>
2020-01-14 01:26:42 +0900

    メソッド取得関数を追加

--- EEG_get.py
+++ EEG_get.py
@@ -1,8 +1,17 @@
 import thinkgear
 
 PORT = '/dev/rfcomm1'
-for packets in thinkgear.ThinkGearProtocol(PORT).get_packets():
-    for p in packets:
-        if isinstance(p, thinkgear.ThinkGearRawWaveData):
-            continue
-        print p
+
+def check_method(terget):
+    print type(terget)
+    for x in dir(terget):
+        print x
+
+if __name__ == "__main__":
+# check_method(thinkgear)
+
+# for packets in thinkgear.ThinkGearProtocol(PORT).get_packets():
+#     for p in packets:
+#         if isinstance(p, thinkgear.ThinkGearRawWaveData):
+#             continue
+#         print p
