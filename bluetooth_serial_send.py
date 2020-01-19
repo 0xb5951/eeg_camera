5c4a82f977641bb62b94b79a4025138cd55102aa
odrum428 <oxkeita@gmail.com>
2020-01-19 21:53:57 +0900

    pythonでble送信するプログラムをテスト実装

---
+++ bluetooth_serial_send.py
@@ -0,0 +1,20 @@
+import bluetooth
+
+mac_address = "D8:A0:1D:51:06:D2"
+central_name = "central"
+connect_port = 1
+
+ble_server = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+while(1):
+    try:
+        print mac_address + " : Connect......"
+        ble_server.connect((mac_address, connect_port))
+        sleep(2)
+        print mac_address + " : Successful Connected!!!"
+        break
+    except bluetooth.BluetoothError:
+        print self.name + " : connecting failed"
+        print "try connecting"
+        sleep(1)
+    except KeyboardInterrupt:
+        break
