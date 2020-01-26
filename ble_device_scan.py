e13bc9d1f8485f954e973f9b9a1b8afe8d81ce64
odrum428 <oxkeita@gmail.com>
2020-01-26 21:26:27 +0900

    データを取得する部分を実装

--- ble_device_scan.py
+++ ble_device_scan.py
@@ -1,13 +1,25 @@
-# coding: utf-8  
+# coding: utf-8
 from bluepy import btle
 
 MAC_ADDRESS = "00:81:F9:29:AE:44"
 
 peripheral = btle.Peripheral(MAC_ADDRESS)
 
-for service in peripheral.getServices():
-    print("serviceUUID: " + str(service.uuid))
-    for characteristic in service.getCharacteristics():
-        print("characteristicUUID: " + str(characteristic.uuid))
-        print("handle: " + str(characteristic.getHandle()))
-        print("property: " + characteristic.propertiesToString())
+
+def get_device_data():
+    for service in peripheral.getServices():
+        print("serviceUUID: " + str(service.uuid))
+        for characteristic in service.getCharacteristics():
+            print("characteristicUUID: " + str(characteristic.uuid))
+            print("handle: " + str(characteristic.getHandle()))
+            print("property: " + characteristic.propertiesToString())
+
+
+def read_characterristics(pointer):
+    data = peripheral.readCharacteristic(pointer)
+    print(str(pointer) + ":" + data)
+
+
+if __name__ == "__main__":
+    for i in range(9, 25):
+        read_characterristics(i)
