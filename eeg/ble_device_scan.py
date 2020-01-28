8c9035edbece9a1f66ae9c917faf4936c3a8b2b6
odrum428 <oxkeita@gmail.com>
2020-01-28 23:48:36 +0900

    デバイスの接続処理をクラス化

--- eeg/ble_device_scan.py
+++ eeg/ble_device_scan.py
@@ -1,36 +1,33 @@
 # coding: utf-8
 from bluepy import btle
 
-MAC_ADDRESS = "D8:A0:1D:51:06:D2"
+class Peripheral:
+    def __init__(self, MAC_ADDRESS):
+        self.peripheral = btle.Peripheral(MAC_ADDRESS)
+    
+    def write_characterristics(self, handle, text):
+        return self.peripheral.writeCharacteristic(handle, text, True)
 
-peripheral = btle.Peripheral(MAC_ADDRESS)
+    def get_device_data(self):
+        for service in self.peripheral.getServices():
+            print("serviceUUID: " + str(service.uuid))
+            for characteristic in service.getCharacteristics():
+                print("characteristicUUID: " + str(characteristic.uuid))
+                print("handle: " + str(characteristic.getHandle()))
+                print("property: " + characteristic.propertiesToString())
 
-
-def get_device_data():
-    for service in peripheral.getServices():
-        print("serviceUUID: " + str(service.uuid))
-        for characteristic in service.getCharacteristics():
-            print("characteristicUUID: " + str(characteristic.uuid))
-            print("handle: " + str(characteristic.getHandle()))
-            print("property: " + characteristic.propertiesToString())
-
-
-def write_characterristics(handle, text):
-    return peripheral.writeCharacteristic(handle, text, True)
-
-
-def read_characterristics(pointer):
-    data = peripheral.readCharacteristic(pointer)
-    # datas = peripheral.getCharacteristics(27,28)
-    print(str(pointer) + ":")
-    print(type(data))
-    print(data)
-
+    def read_characterristics(self, pointer):
+        data = self.peripheral.readCharacteristic(pointer)
+        print(str(pointer) + ":")
+        print(type(data))
+        print(data)
 
 if __name__ == "__main__":
+    MAC_ADDRESS = "D8:A0:1D:51:06:D2"
+    device = Peripheral(MAC_ADDRESS)
     print('start:')
-    get_device_data()
+    device.get_device_data()
     for i in range(42, 43):
-        read_characterristics(i)
+        device.read_characterristics(i)
 
-    write_characterristics(42, "test")
+    device.write_characterristics(42, "test")
